cb6fb7eb6d1851d33fda93884a1b347aeb41482d
AlessandroDiBiase99 <s1104529@studenti.univpm.it>
2023-05-05 09:24:25 +0200

    Adattato risultati

--- AnalisiSistemaComposto_2.m
+++ AnalisiSistemaComposto_2.m
@@ -20,39 +20,39 @@ IP3 = AnalizzaSistema_2(  '8,9,10',Precisione,IP2.TPU_OUT);
 fprintf("-> Gruppo 4\n");
 IP4 = AnalizzaSistema_2('11,12,13',Precisione,IP3.TPU_OUT);
 
-fprintf("%f -> 1,2,3,4 ->%f\n " ,IP1.TPU_IN,IP1.TPU_OUT);
-fprintf("%f -> 5,6 ->%f\n "     ,IP2.TPU_IN,IP2.TPU_OUT);
+% fprintf("%f -> 1,2,3,4 ->%f\n " ,IP1.TPU_IN,IP1.TPU_OUT);
+% fprintf("%f -> 5,6 ->%f\n "     ,IP2.TPU_IN,IP2.TPU_OUT);
 fprintf("%f -> 8,9,10 ->%f\n "  ,IP3.TPU_IN,IP3.TPU_OUT);
 fprintf("%f -> 11,12,13 ->%f\n\n ",IP4.TPU_IN,IP4.TPU_OUT);
 
-if IP4.TPU_IN < IP3.TPU_OUT 
-    fprintf("<- Gruppo 3 <-\n");
-    IP3 = AnalizzaSistema_2(  '8,9,10',Precisione);
-end
-if IP3.TPU_IN < IP2.TPU_OUT
-    fprintf("<- Gruppo 2 <-\n");
-    IP2 = AnalizzaSistema_2(     '5,6',Precisione);
-end
-if IP2.TPU_IN < IP1.TPU_OUT
-    fprintf("Gruppo 1 <-\n");
-    IP1 = AnalizzaSistema_2( '1,2,3,4',Precisione);
-end
+% if IP4.TPU_IN < IP3.TPU_OUT 
+%     fprintf("<- Gruppo 3 <-\n");
+%     IP3 = AnalizzaSistema_2(  '8,9,10',Precisione);
+% end
+% if IP3.TPU_IN < IP2.TPU_OUT
+%     fprintf("<- Gruppo 2 <-\n");
+%     IP2 = AnalizzaSistema_2(     '5,6',Precisione);
+% end
+% if IP2.TPU_IN < IP1.TPU_OUT
+%     fprintf("Gruppo 1 <-\n");
+%     IP1 = AnalizzaSistema_2( '1,2,3,4',Precisione);
+% end
 
 clear Precisione
 
 %% RISULTATI
 % Mancano i nastri trasportatori di collegamento e M7
-Transizioni=[IP1.Transizioni; IP2.Transizioni; IP3.Transizioni; IP4.Transizioni];
+Transizioni=[IP3.Transizioni; IP4.Transizioni];
 
-Posti=[IP1.Posti; IP2.Posti; IP3.Posti; IP4.Posti];
+Posti=[IP3.Posti; IP4.Posti];
 
-WIP=IP1.WIP+IP2.WIP+IP3.WIP+IP4.WIP;
+WIP=IP3.WIP+IP4.WIP;
 
-MLT=IP1.MLT+IP2.MLT+IP3.MLT+IP4.MLT;
+MLT=IP3.MLT+IP4.MLT;
 
 TPU=IP4.TPU_OUT;
 
-EFF=[IP1.Macchinari;IP2.Macchinari;IP3.Macchinari;IP4.Macchinari];
+EFF=[IP3.Macchinari;IP4.Macchinari];
 
 %% RAGGRUPPAMENTI
 Dati=load('Dati\PN_Configurazione2.mat','PN');
