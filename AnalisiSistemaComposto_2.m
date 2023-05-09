917d4c3b621647a4b5b7577d7424ee8eb949ae75
AlessandroDiBiase99 <s1104529@studenti.univpm.it>
2023-05-09 09:26:27 +0200

    Aggiornamento gestoreanalisipn1

--- AnalisiSistemaComposto_2.m
+++ AnalisiSistemaComposto_2.m
@@ -10,18 +10,17 @@ Precisione.U  = 65;
 Precisione.U1 = 65;
 
 %% CALCOLO INDICI DI PRESTAZIONE
-% fprintf("Gruppo 1 ->\n");
-% IP1 = AnalizzaSistema_2( '1,2,3,4',Precisione,0);
-% fprintf("-> Gruppo 2 ->\n");
-% IP2 = AnalizzaSistema_2(     '5,6',Precisione,IP1.TPU_OUT);
-IP2.TPU_OUT=256;
+fprintf("Gruppo 1 ->\n");
+IP1 = AnalizzaSistema_2( '1,2,3,4',Precisione,0);
+fprintf("-> Gruppo 2 ->\n");
+IP2 = AnalizzaSistema_2(     '5,6',Precisione,IP1.TPU_OUT);
 fprintf("-> Gruppo 3 ->\n");
 IP3 = AnalizzaSistema_2(  '8,9,10',Precisione,IP2.TPU_OUT);
 fprintf("-> Gruppo 4\n");
 IP4 = AnalizzaSistema_2('11,12,13',Precisione,IP3.TPU_OUT);
 
-% fprintf("%f -> 1,2,3,4 ->%f\n " ,IP1.TPU_IN,IP1.TPU_OUT);
-% fprintf("%f -> 5,6 ->%f\n "     ,IP2.TPU_IN,IP2.TPU_OUT);
+fprintf("%f -> 1,2,3,4 ->%f\n " ,IP1.TPU_IN,IP1.TPU_OUT);
+fprintf("%f -> 5,6 ->%f\n "     ,IP2.TPU_IN,IP2.TPU_OUT);
 fprintf("%f -> 8,9,10 ->%f\n "  ,IP3.TPU_IN,IP3.TPU_OUT);
 fprintf("%f -> 11,12,13 ->%f\n\n ",IP4.TPU_IN,IP4.TPU_OUT);
 
@@ -42,17 +41,17 @@ clear Precisione
 
 %% RISULTATI
 % Mancano i nastri trasportatori di collegamento e M7
-Transizioni=[IP3.Transizioni; IP4.Transizioni];
+Transizioni=[IP1.Transizioni; IP2.Transizioni; IP3.Transizioni; IP4.Transizioni];
 
-Posti=[IP3.Posti; IP4.Posti];
+Posti=[IP1.Posti; IP2.Posti; IP3.Posti; IP4.Posti];
 
-WIP=IP3.WIP+IP4.WIP;
+WIP=IP1.WIP+IP2.WIP+IP3.WIP+IP4.WIP;
 
-MLT=IP3.MLT+IP4.MLT;
+MLT=IP2.MLT+IP2.MLT+IP3.MLT+IP4.MLT;
 
 TPU=IP4.TPU_OUT;
 
-EFF=[IP3.Macchinari;IP4.Macchinari];
+EFF=[IP1.Macchinari;IP2.Macchinari; IP3.Macchinari;IP4.Macchinari];
 
 %% RAGGRUPPAMENTI
 Dati=load('Dati\PN_Configurazione2.mat','PN');
