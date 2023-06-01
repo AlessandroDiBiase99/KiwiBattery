e0f8af818459ecf01d033a4e8622a09bedc7f984
lucac <lucac@LAPTOP-AOFJF1NI>
2023-06-01 12:33:49 +0200

    Modifiche al plot con funzione area

--- AnalizzaTokenIterativo.m
+++ AnalizzaTokenIterativo.m
@@ -9,7 +9,7 @@ codice_macchinario=2;
 PN=load(sprintf('Parti_v%i/PN_M%i.mat',versione, codice_macchinario));
 
 token.init=2;
-token.ending=40;
+token.ending=15;
 token.delta=1;
 
 Impostazioni.Precisione.U  = 5;
@@ -20,13 +20,25 @@ Impostazioni.RecuperoGrafo="Si";
 rate_in=[255.5333, 268.3663, 134.4334, 267.9495, 267.9409, 200.9588, 200.9595, 3.1400, 100.4797, 100.4803, 200.9624, 100.4799];
 rate_out=[253.5367, 134.1820, 268.8662, 267.9480, 200.9588, 200.9595, 3.1400, 100.4797, 100.4806, 200.9627, 100.4811, 90.4316];
 
+soglia=0.90;
 
 %% CALCOLO ITERATIVO_______________________________________________________
 plot_tp=Calcolo_Iterativo_PN_Grafo(versione,PN,codice_macchinario,token,rate_in(codice_macchinario),rate_out(codice_macchinario),Impostazioni);
-
+cerca=find(plot_tp(:,2)/rate_out(codice_macchinario)>=soglia,1,'first');
+if ~isempty(cerca)
+   trova_token=(cerca-1)*token.delta+token.init;
+end
+%% PLOT
 figure
 plot(token.init:token.delta:token.ending, plot_tp(:,1), token.init:token.delta:token.ending, plot_tp(:,2))
 figure
-plot(token.init:token.delta:token.ending, plot_tp(:,1)/rate_in(codice_macchinario)*100, token.init:token.delta:token.ending, plot_tp(:,2)/rate_out(codice_macchinario)*100)
-
-find(plot_tp(:,2)/rate_out(codice_macchinario)>=0.96,'first');
+a=[trova_token, token.ending];
+b=[soglia*100,soglia*100];
+area(a,b,100,'FaceColor','green','FaceAlpha',0.3,'LineStyle','none')
+hold on
+plot(token.init:token.delta:token.ending, plot_tp(:,1)/rate_in(codice_macchinario)*100, token.init:token.delta:token.ending, plot_tp(:,2)/rate_out(codice_macchinario)*100,'Color','r')
+hold on
+xline(trova_token,'-.')
+yline(soglia*100,'-.')
+hold on
+plot(trova_token, plot_tp(cerca,2)/rate_out(codice_macchinario)*100,'*','Color','b')
